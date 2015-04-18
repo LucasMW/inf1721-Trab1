1c1160803874c91d51427e079f903bd71384592c
Lucas Menezes <lmenezes@MacBook-Pro-de-Lucas.local>
2015-04-18 20:51:23 -0300

    Initial Commit

---
+++ INF1721Trab1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  INF1721Trab1
+//
+//  Created by Lucas Menezes on 18/04/15.
+//  Copyright (c) 2015 MWAPPTECH. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
